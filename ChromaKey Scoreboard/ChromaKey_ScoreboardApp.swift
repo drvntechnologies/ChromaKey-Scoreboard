e6545c53b289a17b098a17857c63aaa1610d5337
Cameron Tarbell <cameron@drvnweb.com>
2024-09-30 14:48:36 -0500

    Initial Commit

---
+++ ChromaKey Scoreboard/ChromaKey_ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaKey_ScoreboardApp.swift
+//  ChromaKey Scoreboard
+//
+//  Created by Cameron Tarbell on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ChromaKey_ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChromaKey ScoreboardTests/ChromaKey_ScoreboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaKey_ScoreboardTests.swift
+//  ChromaKey ScoreboardTests
+//
+//  Created by Cameron Tarbell on 9/30/24.
+//
+
+import Testing
+@testable import ChromaKey_Scoreboard
+
+struct ChromaKey_ScoreboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
